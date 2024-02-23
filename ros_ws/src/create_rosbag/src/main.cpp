413a35b4374ba88b6d29194d794c7498c9291955
larrydong <jfjxxdy2007@126.com>
2024-02-23 16:11:11 +0800

    velodyne pointcloud format output

--- ros_ws/src/create_rosbag/src/main.cpp
+++ ros_ws/src/create_rosbag/src/main.cpp
@@ -12,32 +12,68 @@
 #include <ros/time.h>
 #include <iostream>
 
+
 using namespace std;
+
+
+// convert to velodyne point format.
+struct VelodynePoint{
+    PCL_ADD_POINT4D;
+    float intensity;
+    float time;
+    uint16_t ring;
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+POINT_CLOUD_REGISTER_POINT_STRUCT(VelodynePoint,
+                                  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(float, time, time)(std::uint16_t, ring, ring))
+
+
+#define LIDAR_FREQUENCY (10)
+#define PI (3.1415926f)
+#define OUTPUT_SCAN_LINE (32)         // how many line output.
+const double g_fov_top = 16.0;
+const double g_fov_bottom = -16.0;
+
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "pack_data_to_rosbag");
     ROS_INFO("--> Create rosbag from chrono output.");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
 
-    std::string base_path = "/home/larrydong/Desktop/chrono_output/";
+    std::string base_path = "default";
+    std::string output_bag_filename = "default";
+
+    nh.getParam("input_folder", base_path);
+    nh.getParam("output_file", output_bag_filename);
+
     std::string lidar_folder = base_path + "lidar/";
     std::string imu_path = base_path + "imu.csv";
     std::string gt_path = base_path + "gt.csv";
 
-    std::string output_bag_filename = "/home/larrydong/Desktop/output.bag";
 
     rosbag::Bag bag;
     bag.open(output_bag_filename, rosbag::bagmode::Write);
 
     ros::Time begin_time = ros::Time::now();
+    double init_wait_time = 3.0;            // wait 3s to start recording.
 
     ROS_INFO_STREAM("Input folder: " << base_path);
+    ROS_INFO_STREAM("Lidar folder: " << lidar_folder);
+    ROS_INFO_STREAM("IMU file: " << imu_path);
+    ROS_INFO_STREAM("GT file: " << gt_path);
     ROS_INFO_STREAM("Output file: " << output_bag_filename);
+    ROS_INFO_STREAM("Wait time: " << init_wait_time <<" s");
+
 
     // Load and write Lidar data
-    float lidar_frequency = 10.0;           // lidar 10Hz.
     int lidar_scan_counter = 0;
+    const double vertical_angle_resolution = (g_fov_top - g_fov_bottom)/(OUTPUT_SCAN_LINE-1);
     for (int i = 0;; ++i) {
+        // skip init time.
+        double dt = i * 1.0 / LIDAR_FREQUENCY;
+        if(dt < init_wait_time)
+            continue;
+
         std::string file_name = lidar_folder + "frame_" + std::to_string(i) + ".csv";
         std::ifstream lidar_file(file_name);
         if (!lidar_file) {
@@ -46,33 +82,60 @@ int main(int argc, char** argv) {
             ROS_WARN_STREAM("Estimated simulation time: " << i / 10.0f << "s.");
             break;
         }
+        
         pcl::PointCloud<pcl::PointXYZI> cloud;
+        pcl::PointCloud<VelodynePoint> velo_cloud;
+
         std::string line;
         std::getline(lidar_file, line); // Skip the first empty line
         while (std::getline(lidar_file, line)) {
             std::istringstream iss(line);
             std::string token;
-            pcl::PointXYZI point;
-            std::getline(iss, token, ','); point.x = std::stof(token);
-            std::getline(iss, token, ','); point.y = std::stof(token);
-            std::getline(iss, token, ','); point.z = std::stof(token);
-            std::getline(iss, token, ','); point.intensity = std::stof(token);
-            cloud.push_back(point);
+            // pcl::PointXYZI point;
+            VelodynePoint vp;
+            double x,y,z,intensity;
+
+            std::getline(iss, token, ','); x = std::stof(token);
+            std::getline(iss, token, ','); y = std::stof(token);
+            std::getline(iss, token, ','); z = std::stof(token);
+            std::getline(iss, token, ','); intensity = std::stof(token);
+
+            if(abs(x)<0.01 || abs(y) < 0.01 || std::isnan(x) || std::isnan(y) || isnan(z))            // skip too-near points.
+                continue;
+
+            vp.x = x;
+            vp.y = y;
+            vp.z = z;
+            vp.intensity = intensity;
+            const double rad2deg = 180 / PI;
+            double pitch = atan2(z, sqrt(x * x + y * y)) * rad2deg;
+            int ring = int((pitch - g_fov_bottom) / vertical_angle_resolution);
+            // cout << "i: " << i << ", x: " << x << ", y: " << y << ", z: " << z << ", pitch: " << pitch << ", ring" << ring << endl;
+            assert(ring >= 0 && ring < OUTPUT_SCAN_LINE);
+            vp.ring = ring;
+            vp.time = 0.0;           // no offset time.
+
+            // cloud.push_back(point);
+            velo_cloud.push_back(vp);
         }
-        lidar_scan_counter++;
 
         sensor_msgs::PointCloud2 output;
-        pcl::toROSMsg(cloud, output);
+        pcl::toROSMsg(velo_cloud, output);
         output.header.frame_id = "chrono";
-        output.header.stamp = begin_time + ros::Duration(i * 1.0 / lidar_frequency);
+        lidar_scan_counter++;
+
+        output.header.stamp = begin_time + ros::Duration(dt);
         bag.write("/lidar", output.header.stamp, output);
     }
     ROS_INFO_STREAM("<-- Write Lidar scans: " << lidar_scan_counter);
 
+
+
     // Load and write IMU data
+
     std::ifstream imu_file(imu_path);
     if(!imu_file.is_open()){
-        cout <<"Error. Cannot open IMU file: "<< imu_path << endl;
+        ROS_ERROR_STREAM("Error. Cannot open IMU file: "<< imu_path);
         std::abort();
     }
     int imu_counter = 0;
@@ -84,6 +147,10 @@ int main(int argc, char** argv) {
         std::string token;
         std::getline(iss, token, ','); 
         double dt = std::stod(token);
+
+        if(dt < init_wait_time)     // skip init time;
+            continue;
+
         imu_msg.header.stamp = begin_time + ros::Duration(dt);
         imu_msg.header.frame_id = "chrono";
         std::getline(iss, token, ','); imu_msg.linear_acceleration.x = std::stod(token);
@@ -97,8 +164,13 @@ int main(int argc, char** argv) {
     }
     ROS_INFO_STREAM("<-- Write IMU data: " << imu_counter);
 
+
     // Read and pack GT data
     std::ifstream gt_file(gt_path);
+    if(!gt_file.is_open()){
+        ROS_ERROR_STREAM("Error. Cannot open GT file: "<< gt_path);
+        std::abort();
+    }
     std::getline(gt_file, line); // Skip header
     size_t gt_counter = 0;
     while (std::getline(gt_file, line)) {
@@ -107,6 +179,10 @@ int main(int argc, char** argv) {
         std::string token;
         std::getline(iss, token, ','); 
         double dt = std::stod(token);
+
+        if(dt<init_wait_time)
+            continue;
+
         odom_msg.header.stamp = begin_time + ros::Duration(dt);
         odom_msg.header.frame_id = "chrono";
         std::getline(iss, token, ','); odom_msg.pose.pose.position.x = std::stod(token);
